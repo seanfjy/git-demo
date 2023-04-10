14f108a27ac228747124be8a3040d0b4bca3b2fa
fjy <seanfjy@qq.com>
2023-04-10 20:47:43 +0800

    juc加深学习-lockSupport使用

--- Lession20-JUC/src/main/java/com/fanstudy/juc/LockSupport/LockSupportDemo.java
+++ Lession20-JUC/src/main/java/com/fanstudy/juc/LockSupport/LockSupportDemo.java
@@ -10,91 +10,103 @@ import java.util.concurrent.locks.ReentrantLock;
  * @auther zzyy
  * @create 2022-01-20 16:14
  */
-public class LockSupportDemo
-{
+public class LockSupportDemo {
     static int x = 0;
     static int y = 0;
 
-    public static void main(String[] args)
-    {
-        Thread t1 = new Thread(() -> {
-            try { TimeUnit.SECONDS.sleep(3); } catch (InterruptedException e) { e.printStackTrace(); }
-            System.out.println(Thread.currentThread().getName() + "\t ----come in"+System.currentTimeMillis());
-            LockSupport.park();
-            System.out.println(Thread.currentThread().getName() + "\t ----被唤醒"+System.currentTimeMillis());
-        }, "t1");
-        t1.start();
-
-        //暂停几秒钟线程
-        //try { TimeUnit.SECONDS.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
-
-        new Thread(() -> {
-            LockSupport.unpark(t1);
-            System.out.println(Thread.currentThread().getName()+"\t ----发出通知");
-        },"t2").start();
+    public static void main(String[] args) {
+        // syncWaitNotify();
+//        lockAwaitSignal();
+         useLockSupport();
 
     }
 
-    private static void lockAwaitSignal()
-    {
+    private static void useLockSupport() {
+        Thread t1 = new Thread(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + "\t ----come in" + System.currentTimeMillis());
+            LockSupport.park();
+            System.out.println(Thread.currentThread().getName() + "\t ----被唤醒" + System.currentTimeMillis());
+        }, "t1");
+        t1.start();
+
+        // 暂停几秒钟线程
+//         try { TimeUnit.SECONDS.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
+
+        new Thread(() -> {
+            LockSupport.unpark(t1);
+            System.out.println(Thread.currentThread().getName() + "\t ----发出通知");
+        }, "t2").start();
+    }
+
+    private static void lockAwaitSignal() {
         Lock lock = new ReentrantLock();
         Condition condition = lock.newCondition();
 
         new Thread(() -> {
-            try { TimeUnit.SECONDS.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
+            // try { TimeUnit.SECONDS.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
             lock.lock();
-            try
-            {
-                System.out.println(Thread.currentThread().getName()+"\t ----come in");
+            try {
+                System.out.println(Thread.currentThread().getName() + "\t ----come in");
                 condition.await();
-                System.out.println(Thread.currentThread().getName()+"\t ----被唤醒");
+                System.out.println(Thread.currentThread().getName() + "\t ----被唤醒");
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } finally {
                 lock.unlock();
             }
-        },"t1").start();
+        }, "t1").start();
 
-        //暂停几秒钟线程
-        //try { TimeUnit.SECONDS.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
+        // 暂停几秒钟线程
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         new Thread(() -> {
             lock.lock();
-            try
-            {
+            try {
                 condition.signal();
-                System.out.println(Thread.currentThread().getName()+"\t ----发出通知");
-            }finally {
+                System.out.println(Thread.currentThread().getName() + "\t ----发出通知");
+            } finally {
                 lock.unlock();
             }
-        },"t2").start();
+        }, "t2").start();
     }
 
-    private static void syncWaitNotify()
-    {
+    private static void syncWaitNotify() {
         Object objectLock = new Object();
 
         new Thread(() -> {
-            try { TimeUnit.SECONDS.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
-            synchronized (objectLock){
-                System.out.println(Thread.currentThread().getName()+"\t ----come in");
+            // try { TimeUnit.SECONDS.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
+            synchronized (objectLock) {
+                System.out.println(Thread.currentThread().getName() + "\t ----come in");
                 try {
                     objectLock.wait();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                System.out.println(Thread.currentThread().getName()+"\t ----被唤醒");
+                System.out.println(Thread.currentThread().getName() + "\t ----被唤醒");
             }
-        },"t1").start();
+        }, "t1").start();
 
-        //暂停几秒钟线程
-        //try { TimeUnit.SECONDS.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
+        // 暂停几秒钟线程
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         new Thread(() -> {
-            synchronized (objectLock){
+            synchronized (objectLock) {
                 objectLock.notify();
-                System.out.println(Thread.currentThread().getName()+"\t ----发出通知");
+                System.out.println(Thread.currentThread().getName() + "\t ----发出通知");
             }
-        },"t2").start();
+        }, "t2").start();
     }
 }
