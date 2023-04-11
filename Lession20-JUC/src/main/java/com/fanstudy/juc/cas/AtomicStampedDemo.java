2d2d5c121655d639c7d9766208520d94c0068c63
fjy <seanfjy@qq.com>
2023-04-11 21:58:45 +0800

    juc加深学习-CAS及原子引用,解决ABA

--- Lession20-JUC/src/main/java/com/fanstudy/juc/cas/AtomicStampedDemo.java
+++ Lession20-JUC/src/main/java/com/fanstudy/juc/cas/AtomicStampedDemo.java
@@ -1,6 +1,5 @@
 package com.fanstudy.juc.cas;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,8 +9,7 @@ import java.util.concurrent.atomic.AtomicStampedReference;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-class Book
-{
+class Book {
     private int id;
     private String bookName;
 }
@@ -20,27 +18,26 @@ class Book
  * @auther zzyy
  * @create 2022-02-24 17:34
  */
-public class AtomicStampedDemo
-{
-    public static void main(String[] args)
-    {
-        Book javaBook = new Book(1,"javaBook");
+public class AtomicStampedDemo {
+    public static void main(String[] args) {
+        Book javaBook = new Book(1, "javaBook");
 
-        AtomicStampedReference<Book> stampedReference = new AtomicStampedReference<>(javaBook,1);
+        AtomicStampedReference<Book> stampedReference = new AtomicStampedReference<>(javaBook, 1);
 
-        System.out.println(stampedReference.getReference()+"\t"+stampedReference.getStamp());
+        System.out.println(stampedReference.getReference() + "\t" + stampedReference.getStamp());
 
-        Book mysqlBook = new Book(2,"mysqlBook");
+        Book mysqlBook = new Book(2, "mysqlBook");
 
         boolean b;
-        b = stampedReference.compareAndSet(javaBook, mysqlBook, stampedReference.getStamp(), stampedReference.getStamp() + 1);
+        b = stampedReference.compareAndSet(javaBook, mysqlBook, stampedReference.getStamp(),
+            stampedReference.getStamp() + 1);
 
-        System.out.println(b+"\t"+stampedReference.getReference()+"\t"+stampedReference.getStamp());
+        System.out.println(b + "\t" + stampedReference.getReference() + "\t" + stampedReference.getStamp());
 
+        b = stampedReference.compareAndSet(mysqlBook, javaBook, stampedReference.getStamp(),
+            stampedReference.getStamp() + 1);
 
-        b = stampedReference.compareAndSet(mysqlBook, javaBook, stampedReference.getStamp(), stampedReference.getStamp() + 1);
-
-        System.out.println(b+"\t"+stampedReference.getReference()+"\t"+stampedReference.getStamp());
+        System.out.println(b + "\t" + stampedReference.getReference() + "\t" + stampedReference.getStamp());
 
     }
 }
