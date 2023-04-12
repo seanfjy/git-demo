413936a6165162426148c20a59ade87995b2a414
fjy <seanfjy@qq.com>
2023-04-12 23:55:56 +0800

    juc加深学习-atomic原子类

--- Lession20-JUC/src/main/java/com/fanstudy/juc/atomics/AtomicIntegerArrayDemo.java
+++ Lession20-JUC/src/main/java/com/fanstudy/juc/atomics/AtomicIntegerArrayDemo.java
@@ -6,15 +6,13 @@ import java.util.concurrent.atomic.AtomicIntegerArray;
  * @auther zzyy
  * @create 2022-02-26 10:20
  */
-public class AtomicIntegerArrayDemo
-{
-    public static void main(String[] args)
-    {
+public class AtomicIntegerArrayDemo {
+    public static void main(String[] args) {
         AtomicIntegerArray atomicIntegerArray = new AtomicIntegerArray(new int[5]);
-        //AtomicIntegerArray atomicIntegerArray = new AtomicIntegerArray(5);
-        //AtomicIntegerArray atomicIntegerArray = new AtomicIntegerArray(new int[]{1,2,3,4,5});
+        // AtomicIntegerArray atomicIntegerArray = new AtomicIntegerArray(5);
+        // AtomicIntegerArray atomicIntegerArray = new AtomicIntegerArray(new int[]{1,2,3,4,5});
 
-        for (int i = 0; i <atomicIntegerArray.length(); i++) {
+        for (int i = 0; i < atomicIntegerArray.length(); i++) {
             System.out.println(atomicIntegerArray.get(i));
         }
 
@@ -22,10 +20,10 @@ public class AtomicIntegerArrayDemo
 
         int tmpInt = 0;
 
-        tmpInt = atomicIntegerArray.getAndSet(0,1122);
-        System.out.println(tmpInt+"\t"+atomicIntegerArray.get(0));
+        tmpInt = atomicIntegerArray.getAndSet(0, 1122);
+        System.out.println(tmpInt + "\t" + atomicIntegerArray.get(0));
 
         tmpInt = atomicIntegerArray.getAndIncrement(0);
-        System.out.println(tmpInt+"\t"+atomicIntegerArray.get(0));
+        System.out.println(tmpInt + "\t" + atomicIntegerArray.get(0));
     }
 }
--- Lession20-JUC/src/main/java/com/fanstudy/juc/atomics/LongAdderAPIDemo.java
+++ Lession20-JUC/src/main/java/com/fanstudy/juc/atomics/LongAdderAPIDemo.java
@@ -2,16 +2,13 @@ package com.fanstudy.juc.atomics;
 
 import java.util.concurrent.atomic.LongAccumulator;
 import java.util.concurrent.atomic.LongAdder;
-import java.util.function.LongBinaryOperator;
 
 /**
  * @auther zzyy
  * @create 2022-02-26 18:51
  */
-public class LongAdderAPIDemo
-{
-    public static void main(String[] args)
-    {
+public class LongAdderAPIDemo {
+    public static void main(String[] args) {
         LongAdder longAdder = new LongAdder();
 
         longAdder.increment();
@@ -20,17 +17,10 @@ public class LongAdderAPIDemo
 
         System.out.println(longAdder.sum());
 
-        LongAccumulator longAccumulator = new LongAccumulator(new LongBinaryOperator()
-        {
-            @Override
-            public long applyAsLong(long left, long right)
-            {
-                return left + right;
-            }
-        },0);
+        LongAccumulator longAccumulator = new LongAccumulator((left, right) -> left + right, 0);
 
-        longAccumulator.accumulate(1);//1
-        longAccumulator.accumulate(3);//4
+        longAccumulator.accumulate(1);// 1
+        longAccumulator.accumulate(3);// 4
 
         System.out.println(longAccumulator.get());
     }
