408b99aa379a93719a802b20f077b2bdb38d23c1
fjy <seanfjy@qq.com>
2023-04-14 00:08:09 +0800

    juc加深学习-ThreadLocal

--- Lession20-JUC/src/main/java/com/fanstudy/juc/tl/ReferenceDemo.java
+++ Lession20-JUC/src/main/java/com/fanstudy/juc/tl/ReferenceDemo.java
@@ -5,96 +5,103 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-class MyObject
-{
-    //这个方法一般不用复写，我们只是为了教学给大家演示案例做说明
+class MyObject {
+    // 这个方法一般不用复写，我们只是为了教学给大家演示案例做说明
     @Override
-    protected void finalize() throws Throwable
-    {
+    protected void finalize() throws Throwable {
         // finalize的通常目的是在对象被不可撤销地丢弃之前执行清理操作。
         System.out.println("-------invoke finalize method~!!!");
     }
 }
 
-
 /**
  * @auther zzyy
  */
-public class ReferenceDemo
-{
-    public static void main(String[] args)
-    {
+public class ReferenceDemo {
+    public static void main(String[] args) {
         MyObject myObject = new MyObject();
         ReferenceQueue<MyObject> referenceQueue = new ReferenceQueue<>();
-        PhantomReference<MyObject> phantomReference = new PhantomReference<>(myObject,referenceQueue);
-        //System.out.println(phantomReference.get());
+        PhantomReference<MyObject> phantomReference = new PhantomReference<>(myObject, referenceQueue);
+        // System.out.println(phantomReference.get());
 
         List<byte[]> list = new ArrayList<>();
 
         new Thread(() -> {
-            while (true){
+            while (true) {
                 list.add(new byte[1 * 1024 * 1024]);
-                try { TimeUnit.MILLISECONDS.sleep(500); } catch (InterruptedException e) { e.printStackTrace(); }
-                System.out.println(phantomReference.get()+"\t"+"list add ok");
+                try {
+                    TimeUnit.MILLISECONDS.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(phantomReference.get() + "\t" + "list add ok");
             }
-        },"t1").start();
+        }, "t1").start();
 
+        System.gc();
         new Thread(() -> {
-            while (true){
+            while (true) {
+
                 Reference<? extends MyObject> reference = referenceQueue.poll();
-                if(reference != null){
+                if (reference != null) {
                     System.out.println("-----有虚对象回收加入了队列");
                     break;
                 }
             }
-        },"t2").start();
+        }, "t2").start();
 
     }
 
-
-
-    private static void weakReference()
-    {
+    private static void weakReference() {
         WeakReference<MyObject> weakReference = new WeakReference<>(new MyObject());
-        System.out.println("-----gc before 内存够用： "+weakReference.get());
+        System.out.println("-----gc before 内存够用： " + weakReference.get());
 
         System.gc();
-        //暂停几秒钟线程
-        try { TimeUnit.SECONDS.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
+        // 暂停几秒钟线程
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-        System.out.println("-----gc after 内存够用： "+weakReference.get());
+        System.out.println("-----gc after 内存够用： " + weakReference.get());
     }
 
-    private static void softReference()
-    {
+    private static void softReference() {
         SoftReference<MyObject> softReference = new SoftReference<>(new MyObject());
-        //System.out.println("-----softReference:"+softReference.get());
+        // System.out.println("-----softReference:"+softReference.get());
 
         System.gc();
-        try { TimeUnit.SECONDS.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
-        System.out.println("-----gc after内存够用: "+softReference.get());
-
-        try
-        {
-            byte[] bytes = new byte[20 * 1024 * 1024];//20MB对象
-        }catch (Exception e){
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
             e.printStackTrace();
-        }finally {
-            System.out.println("-----gc after内存不够: "+softReference.get());
+        }
+        System.out.println("-----gc after内存够用: " + softReference.get());
+
+        try {
+            byte[] bytes = new byte[20 * 1024 * 1024];// 20MB对象
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("-----gc after内存不够: " + softReference.get());
         }
     }
 
-    private static void strongReference()
-    {
+    private static void strongReference() {
         MyObject myObject = new MyObject();
-        System.out.println("gc before: "+myObject);
+        System.out.println("gc before: " + myObject);
 
         myObject = null;
-        System.gc();//人工开启GC，一般不用
+        System.gc();// 人工开启GC，一般不用
 
-        //暂停毫秒
-        try { TimeUnit.MILLISECONDS.sleep(500); } catch (InterruptedException e) { e.printStackTrace(); }
+        // 暂停毫秒
+        try {
+            TimeUnit.MILLISECONDS.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-        System.out.println("gc after: "+myObject);
+        System.out.println("gc after: " + myObject);
     }
 }
--- Lession20-JUC/src/main/java/com/fanstudy/juc/tl/ThreadLocalDemo.java
+++ Lession20-JUC/src/main/java/com/fanstudy/juc/tl/ThreadLocalDemo.java
@@ -3,11 +3,11 @@ package com.fanstudy.juc.tl;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
-class House //资源类
+class House // 资源类
 {
     int saleCount = 0;
-    public synchronized void saleHouse()
-    {
+
+    public synchronized void saleHouse() {
         ++saleCount;
     }
 
@@ -19,9 +19,9 @@ class House //资源类
         }
     };*/
     ThreadLocal<Integer> saleVolume = ThreadLocal.withInitial(() -> 0);
-    public void saleVolumeByThreadLocal()
-    {
-        saleVolume.set(1+saleVolume.get());
+
+    public void saleVolumeByThreadLocal() {
+        saleVolume.set(1 + saleVolume.get());
     }
 }
 
@@ -29,76 +29,39 @@ class House //资源类
  * @auther zzyy
  * @create 2021-12-31 15:46
  *
- * 需求1： 5个销售卖房子，集团高层只关心销售总量的准确统计数。
+ *         需求1： 5个销售卖房子，集团高层只关心销售总量的准确统计数。
  *
- * 需求2： 5个销售卖完随机数房子，各自独立销售额度，自己业绩按提成走，分灶吃饭，各个销售自己动手，丰衣足食
+ *         需求2： 5个销售卖完随机数房子，各自独立销售额度，自己业绩按提成走，分灶吃饭，各个销售自己动手，丰衣足食
  *
  *
  */
-public class ThreadLocalDemo
-{
-    public static void main(String[] args) throws InterruptedException
-    {
+public class ThreadLocalDemo {
+    public static void main(String[] args) throws InterruptedException {
 
         House house = new House();
 
-        for (int i = 1; i <=5; i++) {
+        for (int i = 1; i <= 5; i++) {
             new Thread(() -> {
-                int size = new Random().nextInt(5)+1;
+                int size = new Random().nextInt(5) + 1;
                 try {
-                    for (int j = 1; j <=size; j++) {
+                    for (int j = 1; j <= size; j++) {
                         house.saleHouse();
                         house.saleVolumeByThreadLocal();
                     }
-                    System.out.println(Thread.currentThread().getName()+"\t"+"号销售卖出："+house.saleVolume.get());
+                    System.out.println(Thread.currentThread().getName() + "\t" + "号销售卖出：" + house.saleVolume.get());
                 } finally {
                     house.saleVolume.remove();
                 }
-            },String.valueOf(i)).start();
-        };
+            }, String.valueOf(i)).start();
+        } ;
 
-        //暂停毫秒
-        try { TimeUnit.MILLISECONDS.sleep(300); } catch (InterruptedException e) { e.printStackTrace(); }
+        // 暂停毫秒
+        try {
+            TimeUnit.MILLISECONDS.sleep(300);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-        System.out.println(Thread.currentThread().getName()+"\t"+"共计卖出多少套： "+house.saleCount);
+        System.out.println(Thread.currentThread().getName() + "\t" + "共计卖出多少套： " + house.saleCount);
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
