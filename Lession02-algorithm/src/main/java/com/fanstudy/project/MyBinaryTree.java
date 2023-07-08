03c1f29b6db94e25e071d0e958cc84b38785b68a
fjy <seanfjy@qq.com>
2023-07-09 00:52:15 +0800

    数据结构,算法学习项目-二叉树遍历

---
+++ Lession02-algorithm/src/main/java/com/fanstudy/project/MyBinaryTree.java
@@ -0,0 +1,116 @@
+package com.fanstudy.project;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Stack;
+
+/**
+ * 二叉树遍历
+ */
+public class MyBinaryTree {
+
+    /**
+     * 二叉树节点
+     */
+    private static class TreeNode {
+        int data;
+        TreeNode leftChild;
+        TreeNode rightChild;
+
+        public TreeNode(int data) {
+            this.data = data;
+        }
+    }
+
+    /**
+     * 构建二叉树
+     * 
+     * @param inputList
+     * @return
+     */
+    public static TreeNode createBinaryTree(LinkedList<Integer> inputList) {
+
+        TreeNode node = null;
+        if (inputList == null || inputList.isEmpty()) {
+            return null;
+        }
+        Integer data = inputList.removeFirst();
+        if (data != null) {
+            node = new TreeNode(data);
+            node.leftChild = createBinaryTree(inputList);
+            node.rightChild = createBinaryTree(inputList);
+        }
+
+        return node;
+    }
+
+    /**
+     * 二叉树前序遍历
+     * 
+     * @param node 二叉树节点
+     */
+    public static void preOrderTraveral(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        System.out.println(node.data);
+        preOrderTraveral(node.leftChild);
+        preOrderTraveral(node.rightChild);
+    }
+
+    // 二叉树中序遍历
+    public static void inOrderTraveral(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        preOrderTraveral(node.leftChild);
+        System.out.println(node.data);
+        preOrderTraveral(node.rightChild);
+    }
+
+    // 二叉树后序遍历
+    public static void PostOrderTraveral(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        preOrderTraveral(node.leftChild);
+        preOrderTraveral(node.rightChild);
+        System.out.println(node.data);
+    }
+
+    /**
+     * 二叉树非递归前序遍历
+     * 
+     * @param root
+     */
+    public static void preOrderTraveralWithStack(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode treeNode = root;
+        while (treeNode != null || !stack.isEmpty()) {
+            if (treeNode != null) {
+                System.out.println(treeNode.data);
+                stack.push(treeNode);
+                treeNode = treeNode.leftChild;
+            } else {
+                treeNode = stack.pop();
+                treeNode = treeNode.rightChild;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedList<Integer> inputList =
+            new LinkedList<>(Arrays.asList(3, 2, 9, null, null, 10, null, null, 8, null, 4));
+        TreeNode treeNode = createBinaryTree(inputList);
+        System.out.println("前序遍历递归: ");
+        preOrderTraveral(treeNode);
+        System.out.println("前序遍历非递归: ");
+        preOrderTraveralWithStack(treeNode);
+        System.out.println("中序遍历递归: ");
+        inOrderTraveral(treeNode);
+        System.out.println("后续遍历递归: ");
+        PostOrderTraveral(treeNode);
+
+    }
+
+}
